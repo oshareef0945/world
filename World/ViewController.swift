ce3c4d905201b5efdf3085cd997ade87b94db53b
Omar Shareef <omar@mb-omars1-2.lan>
2017-06-07 13:48:03 -0400

    Initial Commit

---
+++ World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World
+//
+//  Created by Omar Shareef on 6/7/17.
+//  Copyright © 2017 OSHATech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
